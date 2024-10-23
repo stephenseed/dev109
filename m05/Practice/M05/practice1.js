b2c342cbdb51c96cb76aa8f6f8a581e15d86d50e
stephenseed <stephenrseed@hotmail.com>
2024-10-23 15:14:39 -0700

    Update practice1.js

--- m05/Practice/M05/practice1.js
+++ m05/Practice/M05/practice1.js
@@ -5,16 +5,16 @@ var height = prompt("Please enter you height in meters:");
 var age = prompt("Please enter your age in years:");
 var MBI = weight / (height * height);
 switch(BMI) {
-  case (BMI>=19 & BMI<25):
+  case (BMI>=19 && BMI<25):
     console.log("Your weight is normal");
     break;
-  Case (BMI >=25 & BMI<30):
+  Case (BMI >=25 && BMI<30):
     console.log("You are overweight.");
     Break;
-  Case (BMI >=30 & BMI<35):
+  Case (BMI >=30 && BMI<35):
     console.log("You are Obese Level 1.");   
     break;
-  Case (BMI >=35 & BMI<40):
+  Case (BMI >=35 && BMI<40):
     console.log("You are obese level 2.");
     break;
   Case (BMI >=40):

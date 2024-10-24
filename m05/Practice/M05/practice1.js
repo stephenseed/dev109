3766683002e73514668f4e4b11495af8a50a10ab
stephenseed <stephenrseed@hotmail.com>
2024-10-23 20:43:07 -0700

    Changed case to IF

--- m05/Practice/M05/practice1.js
+++ m05/Practice/M05/practice1.js
@@ -4,23 +4,11 @@ var weight = prompt("Please enter your weight in KG:");
 var height = prompt("Please enter you height in meters:");
 var age = prompt("Please enter your age in years:");
 var MBI = weight / (height * height);
-switch(BMI) {
-  case (BMI>=19 && BMI<25):
-    console.log("Your weight is normal");
-    break;
-  Case (BMI >=25 && BMI<30):
-    console.log("You are overweight.");
-    Break;
-  Case (BMI >=30 && BMI<35):
-    console.log("You are Obese Level 1.");   
-    break;
-  Case (BMI >=35 && BMI<40):
-    console.log("You are obese level 2.");
-    break;
-  Case (BMI >=40):
-    console.log("You are obese Level 3.");
-    break;
-  default:
-    console.log("You are underweight.");
+if (BMI >= 19 && BMI < 25) { console.log("Your weight is normal");
+} else if (BMI >= 25 && BMI < 30) { console.log("You are overweight.");
+} else if (BMI >= 30 && BMI < 35) { console.log("You are Obese Level 1.");
+} else if (BMI >= 35 && BMI < 40) { console.log("You are obese level 2.");
+} else if (BMI >= 40) { console.log("You are obese Level 3.");
+} else { console.log("You are underweight.");
 }
 document.write('<h3>' + "Done" + '</h3>');

17bbd7655355fd832e6d7b29187b6603d4e4d084
stephenseed <stephenrseed@hotmail.com>
2024-10-23 20:51:04 -0700

    Update practice1.js

--- m05/Practice/M05/practice1.js
+++ m05/Practice/M05/practice1.js
@@ -3,7 +3,7 @@ var message;
 var weight = prompt("Please enter your weight in KG:");
 var height = prompt("Please enter you height in meters:");
 var age = prompt("Please enter your age in years:");
-var MBI = weight / (height * height);
+var BMI = weight / (height * height);
 if (BMI >= 19 && BMI < 25) { console.log("Your weight is normal");
 } else if (BMI >= 25 && BMI < 30) { console.log("You are overweight.");
 } else if (BMI >= 30 && BMI < 35) { console.log("You are Obese Level 1.");

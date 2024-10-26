42b14e237c24c9a1537a97fe4a480674f65d5c3a
stephenseed <stephenrseed@hotmail.com>
2024-10-25 22:27:27 -0700

    Update example.js

--- m05/example.js
+++ m05/example.js
@@ -1,14 +1,10 @@
 var table = prompt("Which multiplication table would you like to see?"); // Unit of Table
-var operator = 'addition'; // Type of calculation
-var i = 1;                 // Set counter to 1
 var msg = '<h2>Multiplication Table</h2>';              // Message
-
-
-  while (i < 11) {
+var i = 1;                 // Set counter to 1
+while (i < 11) {
     msg += i + ' x ' + table + ' = ' + (i * table) + '<br />';
     i++;
   }
-
 
 // Write the message into the page
 var el = document.getElementById('blackboard');

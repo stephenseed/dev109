c212ad9001d089ec6ab170cc318e71f391f70e2c
stephenseed <stephenrseed@hotmail.com>
2024-11-08 19:41:35 -0800

    Update multiple-objects.js

--- m07/js/multiple-objects.js
+++ m07/js/multiple-objects.js
@@ -12,6 +12,7 @@ function Hotel(name, rooms, booked) {
 // Create two hotel objects
 var quayHotel = new Hotel('Quay', 40, 25);
 var parkHotel = new Hotel('Park', 120, 77);
+var queenHotel = new Hotel('Queen',66,23);
 
 
 // Update the HTML for the page
@@ -25,6 +26,10 @@ var details2 = parkHotel.name + ' rooms: ';
 var elHotel2 = document.getElementById('hotel2');
 elHotel2.textContent = details2;
 
+var details3 = queenHotel.name + ' rooms: ';
+    details3 += queenHotel.checkAvailability();
+var elHotel2 = document.getElementById('hotel3');
+elHotel3.textContent = details3;
 /* 
 NOTE: textContent does not work in IE8 or earlier
 You can use innerHTML on lines 21 and 26, but note the security issues on p228-231

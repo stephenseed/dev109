f812879ba2857909986ab3feab8fc91b473234ed
stephenseed <stephenrseed@hotmail.com>
2024-12-08 20:14:28 -0800

    Update script.js

--- a3/js/script.js
+++ a3/js/script.js
@@ -7,10 +7,10 @@
 function printRhombus(pHeight,symbol) {
     let rhombus = '';
     for (let i = 0; i < pHeight; i++) {
-        rhombus += ' '.repeat(pHeight - i - 1) + symbol.repeat(2 * i + 1) + '\n';
+        rhombus += ' '.repeat(pHeight - i - 1) + symbol.repeat(2 * i + 1) + <br>;
     }
     for (let i = pHeight - 2; i >= 0; i--) {
-        rhombus += ' '.repeat(pHeight - i - 1) + symbol.repeat(2 * i + 1) + '\n';
+        rhombus += ' '.repeat(pHeight - i - 1) + symbol.repeat(2 * i + 1) + <br>;
     }
     document.getElementById('rhombus-container').innerHTML = rhombus;
 

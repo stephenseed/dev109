f07bd0d59e0d087dfb9d46d97fe4df6f14377693
stephenseed <stephenrseed@hotmail.com>
2024-12-09 18:14:53 -0800

    Update script.js

--- a4/js/script.js
+++ a4/js/script.js
@@ -1,1 +1,12 @@
+function validateForm(){
+//1) create a variable to control status of each field. Assume that they are not valid
 
+//2) create variables to read the values from html text inputs
+
+//3) do the validation
+
+//4) send error messages 
+
+//5) return the status of each field
+
+}

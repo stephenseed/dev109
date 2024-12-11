ce6dbbc5389a983d61e0fbd6aabc70a1c6a9bf56
stephenseed <stephenrseed@hotmail.com>
2024-12-10 17:46:08 -0800

    Update script.js

--- a4/js/script.js
+++ a4/js/script.js
@@ -1,6 +1,12 @@
 function validateForm(){
 //1) create a variable to control status of each field. Assume that they are not valid
-
+var valid;
+var fieldNames = ['Firstname','Lastname','
+var answer;
+for (var i=0; i <12; i++) {
+  valid[i]=false;
+}
+    
 //2) create variables to read the values from html text inputs
 
 //3) do the validation

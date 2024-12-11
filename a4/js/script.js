51382adf01f812cb198fb6ec5ead161c5ddd77d0
stephenseed <stephenrseed@hotmail.com>
2024-12-11 14:30:40 -0800

    Update script.js

--- a4/js/script.js
+++ a4/js/script.js
@@ -3,7 +3,6 @@ function validateForm(){
 var valid = [];
 var fieldNames = ['FirstName','Lastname','email','Phone','Username','Password','Address','City','State','Country','ZipCode','Comments'];
 var answer = [];
-var errorMessages = "";
 
 for (var i=0; i < fieldNames.length; i++) {
   valid[i]=false;
@@ -16,97 +15,108 @@ for (var i=0; i < fieldNames.length; i++) {
 
 // First Name Validation
 if (answer[0]==="null" || answer[0]==="" || answer[0].length > 20) {
-    errorMessages += "<p>The firstname is required and cannot be greater than 20 characters</p>";
     document.getElementById("FirstNameError").innerHTML = "<p style='color:red;'><b>The firstname is required and cannot be greater than 20 characters</b></p>";
 }
 else {
+   document.getElementById("FirstNameError").innerHTML = "";
    valid[0] = true;
 }
 // Last Name Validation
   if (answer[1]==="null" || answer[1]==="" || answer[1].length > 50) {
-    errorMessages += "<p>The lastname is required and cannot be greater than 50 characters</p>";
+    document.getElementById("LastnameError").innerHTML = "<p style='color:red;'><b>The lastname is required and cannot be greater than 20 characters</b></p>";
   }
-else {
+  else {
    valid[1] = true;
-}
+   document.getElementById("LastnameError").innerHTML = "";
+  }
   
 // e-mail validation
 var atpos = answer[2].indexOf("@");
 var dotpos = answer[2].lastIndexOf(".");
 if (atpos< 1 || dotpos<atpos+2 || dotpos+2>=answer[2].length) {
-    errorMessages += "<p>Invalid e-mail address - must contain a . and @ sign.<p>";
+    document.getElementById("emailError").innerHTML = "<p style='color:red;'>"<p>Invalid e-mail address - must contain a . and @ sign.<p>";
 }
 else {
   valid[2] = true
+  document.getElementById("emailError").innerHTML = "";
 }
   
 // Phone validation
 if (isNaN(answer[3]) || answer[3].length >15 || answer[3]===null || answer[3]==="") {
-  errorMessages += "<p>Invalid Phone Number or must be provided.</p>";
+  document.getElementById("PhoneError").innerHTML = "<p style='color:red;'>"<p>Invalid Phone Number or must be provided.</p>";
 }
 else {
   valid[3]=true;
+  document.getElementById("PhoneError").innerHTML = "";
 }
 
 // Username validation
 if (answer[4].length > 12 || answer[4]===null || answer[4]==="") {
-  errorMessages += "<p>The username is required.</p>";
+  document.getElementById("UsernameError").innerHTML = "<p style='color:red;'>"<p>The username is required.</p>";
 }
   else {
     valid[4]=true;
+    document.getElementById("UsernameError").innerHTML = "";
   }
 
 // Password Validation
   if (answer[5].length < 7 || answer[5]===null || answer[5]==="") {
-  errorMessages += "<p>The password is required.</p>";
+  document.getElementById("PasswordError").innerHTML = "<p style='color:red;'>"<p>The password is required.</p>";
 }
   else {
     valid[5]=true;
+    document.getElementById("PasswordError").innerHTML = "";
   }
 
 // Address validation
   if (answer[6]===null || answer[6]==="") {
-  errorMessages += "<p>The address is required.</p>";
+  document.getElementById("AddressError").innerHTML = "<p style='color:red;'>"<p>The address is required.</p>";
 }
   else {
     valid[6]=true;
+    document.getElementById("AddressError").innerHTML = "";
   }
 
 // City Validation
   if (answer[7]===null || answer[7]==="") {
-  errorMessages += "<p>The city is required.</p>";
+  document.getElementById("CityError").innerHTML = "<p style='color:red;'>"<p>The city is required.</p>";
 }
   else {
     valid[7]=true;
+    document.getElementById("CityError").innerHTML = "";
   }
 
 // State Validation
   if (answer[8]===null || answer[8]==="") {
-  errorMessages += "<p>The state is required.</p>";
+  document.getElementById("StateError").innerHTML = "<p style='color:red;'>"<p>The state is required.</p>";
 }
   else {
     valid[8]=true;
+    document.getElementById("StateError").innerHTML = "";
   }
 
 // Country Validation
   if (answer[9]===null || answer[9]==="") {
-  errorMessages += "<p>The Country is required.</p>";
+  document.getElementById("CountryError").innerHTML = "<p style='color:red;'>"<p>The Country is required.</p>";
 }
   else {
     valid[9]=true;
+    document.getElementById("CountryError").innerHTML = "";
   }
 
 // Zip Code Validation
   if (answer[9]==="USA") {
     if (answer[10].length >5 || answer[10]===null || answer[10]==="") {
-    errorMessages += "<p>The zipcode is required.</p>";
+    document.getElementById("ZipCodeError").innerHTML = "<p style='color:red;'>"<p>The zipcode is required.</p>";
     }
     else {
       valid[10]=true;
+      document.getElementById("ZipCodeError").innerHTML = "";
     }
   }
   else {
     valid[10]=true;
+    document.getElementById("ZipCodeError").innerHTML = "";
   }
   
 // Comment Validation is NOT required.

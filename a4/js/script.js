ee80cec1fd0ffe8d90596e2a3f52d26aa49fe385
stephenseed <stephenrseed@hotmail.com>
2024-12-10 23:24:40 -0800

    Update script.js

--- a4/js/script.js
+++ a4/js/script.js
@@ -28,6 +28,7 @@ else {
 else {
    valid[1] = true;
 }
+  
 // e-mail validation
 var atpos = answer[2].indexOf("@");
 var dotpos = uanswer[2].lastIndexOf(".");
@@ -37,7 +38,66 @@ if (atpos< 1 || dotpos<atpos+2 || dotpos+2>=userEmail.length) {
 else {
   valid[2] = true
 }
+  
+// Phone validation
+if (isNaN(answer[3]) || answer[3].length >15 || answer[3]===null || answer[3]==="") {
+  errorMessages += "<p>Invalid Phone Number or must be provided.</p>";
+}
+else {
+  valid[3]=true;
+}
 
+// Username validation
+if (answer[4].length > 12 || answer[4]===null || answer[4]==="") {
+  errorMessages += "<p>The username is required.</p>";
+}
+  else {
+    valid[4]=true;
+  }
+
+// Password Validation
+  if (answer[5].length > 7 || answer[5]===null || answer[5]==="") {
+  errorMessages += "<p>The password is required.</p>";
+}
+  else {
+    valid[5]=true;
+  }
+
+// Address validation
+  if (answer[6]===null || answer[6]==="") {
+  errorMessages += "<p>The address is required.</p>";
+}
+  else {
+    valid[6]=true;
+  }
+
+// City Validation
+  if (answer[7]===null || answer[7]==="") {
+  errorMessages += "<p>The city is required.</p>";
+}
+  else {
+    valid[7]=true;
+  }
+
+// State Validation
+  if (answer[8]===null || answer[8]==="") {
+  errorMessages += "<p>The state is required.</p>";
+}
+  else {
+    valid[8]=true;
+  }
+
+
+  
+
+// Country Validation
+  if (answer[9]===null || answer[9]==="") {
+  errorMessages += "<p>The Country is required.</p>";
+}
+  else {
+    valid[9]=true;
+  }
+  
   
 //--------------------------------------------------------------------------------------//
 //4) send error messages 
